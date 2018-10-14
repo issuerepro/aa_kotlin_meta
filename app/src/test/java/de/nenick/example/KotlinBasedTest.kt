f1c50f71864122943b892c4f439cd687f0019629
Kay-Uwe Janssen <git@kujanssen.de>
2018-10-14 10:12:13 +0200

    initial commit

---
+++ app/src/test/java/de/nenick/example/KotlinBasedTest.kt
@@ -0,0 +1,25 @@
+package de.nenick.example
+
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class KotlinBasedTest {
+
+    @Test
+    fun androidAnnotations() {
+        val activity = MainActivity_()
+        activity.setContentView(null)
+        // stacktrace has no info about our activity
+    }
+
+    @Test
+    fun androidAnnotationsCompile() {
+        val activity = MainActivity_()
+        // following line does not compile
+//        activity.onViewChanged(null)
+    }
+}
